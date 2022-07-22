c4e7d9708c7d00f5f7b736f9893ec923d815bd3e
John Willman <johnpwillman@gmail.com>
2022-07-22 16:10:04 -0500

    Blueprints section of tutorial

--- flaskr/db.py
+++ flaskr/db.py
@@ -26,6 +26,7 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+# Register 'init-db' as flask command
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
